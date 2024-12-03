d99e8d662461cb165df0cd4c19235d8ec3578344
charkhaniakash <charkhaniakash@gmail.com>
2024-12-04 00:02:51 +0530

    added condition while creating post

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -33,6 +33,10 @@ module.exports = {
       const user = checkAuth(context);
       console.log(user);
 
+      if(args.body.trim() === ""){
+        throw new UserInputError('Post body must not be empty');
+      }
+
       const newPost = new Post({
         body,
         user: user.id,
